74079efadaa75e167e11c70a21d84349ccecdb07
Daniel Callisaya <daniel_jch@hotmail.com>
2023-06-14 20:10:42 -0400

    Initial Commit

---
+++ TImeConversion/TImeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TImeConversionApp.swift
+//  TImeConversion
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TImeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
